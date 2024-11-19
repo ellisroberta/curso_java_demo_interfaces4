72f00539966e37d83804d5f0c697d4cf01aec00c
ellisroberta <ellisroberta@hotmail.com>
2024-11-19 15:52:25 -0300

    adicao de comentario

--- application/src/application/Program.java
+++ application/src/application/Program.java
@@ -1,3 +1,8 @@
+/** Problema motivador
+ Faça um programa para ler um arquivo contendo nomes de pessoas (um nome por
+ linha), armazenando-os em uma lista. Depois, ordenar os dados dessa lista e mostra-los
+ ordenadamente na tela. Nota: o caminho do arquivo pode ser informado "hardcode". **/
+
 package application;
 
 import java.io.BufferedReader;
